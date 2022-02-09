5cfcdde02cb8458f57e67011e8358b82d2088b34
Lachlan Munro <lachlangmunro@gmail.com>
2022-02-09 20:21:55 +1100

    Compelted Function Expression exercise.

---
+++ Javascript/Functions/Level Up Functions/Function Expressions/app.js
@@ -0,0 +1,17 @@
+//FUNCTION EXPRESSIONS
+
+//Standard function.
+function add(x, y) {
+    return x + y;
+}
+
+add(3, 4)
+
+//Store function within variable.
+const add = function (x, y) {
+    return x + y;
+}
+
+add(3, 4)
+
+
