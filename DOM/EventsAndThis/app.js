22213098c3e098fdee41a03f69102af9e3963b06
Lachlan Munro <lachlangmunro@gmail.com>
2022-03-02 15:49:34 +1100

    Completed Events and This exercise.

---
+++ DOM/EventsAndThis/app.js
@@ -0,0 +1,24 @@
+const randomColour = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r},${g},${b})`
+}
+
+function colorize() {
+  this.style.backgroundColor = randomColour(); //usage of keyword this.
+  this.style.color = randomColour();
+}
+
+const buttons = document.querySelectorAll('button');
+
+for(let button of buttons){
+  button.addEventListener('click', colorize)
+}
+
+
+const h1s = document.querySelectorAll('h1');
+
+for(let h1 of h1s){
+  h1.addEventListener('click', colorize)
+}
