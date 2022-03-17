19ba56146136526b28beb818250413f0a16b3a21
Lachlan Munro <lachlangmunro@gmail.com>
2022-03-17 16:02:31 +1100

    Completed Change and Input Events exercise.

---
+++ DOM/ChangeAndInputEvents/app.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('input');
+const h1 = document.querySelector('h1');
+
+// input.addEventListener('change', function (e) { //only once user clicks out of input
+//   console.log("Bingo Bango")
+// })
+
+input.addEventListener('input', function (e) { //when user types
+  h1.innerText = input.value
+})
+
+
+//Coding Exercise 62: Input Event Practice
+const input = document.querySelector("input");
+const h1 = document.querySelector("h1");
+
+input.addEventListener('input', function (e) {
+    if (!input.value) {
+            h1.innerText = "Enter Your Username";
+    } else {
+           h1.innerText = `Welcome, ${input.value}`
+    }
+})
