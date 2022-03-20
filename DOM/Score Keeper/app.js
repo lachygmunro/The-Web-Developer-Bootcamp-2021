32cff0b28c2ceae224334ce14e90c699ebdfe9b2
Lachlan Munro <lachlangmunro@gmail.com>
2022-03-20 12:59:14 +1100

    Completed score keeper.

--- DOM/Score Keeper/app.js
+++ DOM/Score Keeper/app.js
@@ -2,7 +2,7 @@ const p1Button = document.querySelector("#p1Button")
 const p2Button = document.querySelector("#p2Button")
 const p1Display = document.querySelector("#p1Display")
 const p2Display = document.querySelector("#p2Display")
-const reset = document.querySelector("#reset")
+const resetButton = document.querySelector("#reset")
 const winningScoreSelect = document.querySelector("#playto")
 
 let p1Score = 0;
@@ -11,26 +11,28 @@ let winningScore = 5;
 let gameOver = false;
 
 p1Button.addEventListener('click', function () {
-  alert("p1!")
   if(!gameOver){
     p1Score += 1;
     if(p1Score == winningScore){
       gameOver = true;
-      p1Display.classList.add('winner')
-      p2Display.classList.add('loser')
+      p1Display.classList.add('has-text-success')
+      p2Display.classList.add('has-text-danger')
+      p1Button.disabled = true;
+      p2Button.disabled = true;
     }
     p1Display.textContent = p1Score;
   }
 })
 
 p2Button.addEventListener('click', function () {
-  alert("p2!")
   if(!gameOver){
     p2Score += 1;
     if(p2Score == winningScore){
       gameOver = true;
-      p2Display.classList.add('winner')
-      p1Display.classList.add('loser')
+      p2Display.classList.add('has-text-success')
+      p1Display.classList.add('has-text-danger')
+      p1Button.disabled = true;
+      p2Button.disabled = true;
     }
     p2Display.textContent = p2Score;
   }
@@ -41,7 +43,7 @@ winningScoreSelect.addEventListener('change', function () {
   reset()
 })
 
-reset.addEventListener('click', reset)
+resetButton.addEventListener('click', reset)
 
 function reset() {
   p1Score = 0;
@@ -49,6 +51,8 @@ function reset() {
   p2Score = 0;
   p2Display.textContent = p2Score;
   gameOver = false;
-  p1Display.classList.remove('winner', 'loser')
-  p2Display.classList.remove('winner', 'loser')
+  p1Display.classList.remove('has-text-success', 'has-text-danger')
+  p2Display.classList.remove('has-text-success', 'has-text-danger')
+  p1Button.disabled = false;
+  p2Button.disabled = false;
 }
